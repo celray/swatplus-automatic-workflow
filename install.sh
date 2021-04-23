4259d81b3e19719e1f6ebb6fa18d0f3d577d8110
jean pierre huart <j.huart@cra.wallonie.be>
2021-04-23 15:03:19 +0200

    customize install dir in my conda environment

--- install.sh
+++ install.sh
@@ -11,12 +11,14 @@
 #
 # set version to download
 version="1.0.4"
+# set swat home
+export SWAT_HOME="/home/jph/dev/conda/pesteaux_conda/swat_tools"
 
-rm -r "$HOME/.SWAT/SWATPlus"
-rm -r "$HOME/.local/share/swatplus"
+rm -r "$SWAT_HOME/.SWAT/SWATPlus"
+rm -r "$SWAT_HOME/.local/share/swatplus"
 
-mkdir -p "$HOME/.SWAT/SWATPlus"
-mkdir -p "$HOME/.local/share/swatplus"
+mkdir -p "$SWAT_HOME/.SWAT/SWATPlus"
+mkdir -p "$SWAT_HOME/.local/share/swatplus"
 
 wget -c "https://github.com/celray/swatplus-automatic-workflow/archive/v$version.zip"
 wget -c "https://github.com/celray/swatplus-automatic-workflow/releases/download/v$version/TauDEM5Bin_Linux.zip"
@@ -27,22 +29,22 @@ unzip "./v$version.zip"
 unzip "./TauDEM5Bin_Linux.zip"
 
 echo "copying files"
-mv "./swatplus-automatic-workflow-$version" "$HOME/.SWAT/SWATPlus/Workflow"
-mv "./TauDEM5Bin" "$HOME/.local/share/swatplus/TauDEM5Bin"
-mv "./swatplus_wgn.sqlite" "$HOME/.SWAT/SWATPlus/Workflow/editor_api/"
-chmod 777 "$HOME/.SWAT/SWATPlus/Workflow/swatplus_aw.sh"
-chmod -R 777 "$HOME/.local/share/swatplus/TauDEM5Bin"
+mv "./swatplus-automatic-workflow-$version" "$SWAT_HOME/.SWAT/SWATPlus/Workflow"
+mv "./TauDEM5Bin" "$SWAT_HOME/.local/share/swatplus/TauDEM5Bin"
+mv "./swatplus_wgn.sqlite" "$SWAT_HOME/.SWAT/SWATPlus/Workflow/editor_api/"
+chmod 777 "$SWAT_HOME/.SWAT/SWATPlus/Workflow/swatplus_aw.sh"
+chmod -R 777 "$SWAT_HOME/.local/share/swatplus/TauDEM5Bin"
 
 # cleanup
 echo "cleaning up"
-rm "$HOME/.SWAT/SWATPlus/Workflow/.gitattributes"
-rm "$HOME/.SWAT/SWATPlus/Workflow/swatplus_aw.bat"
-rm "$HOME/.SWAT/SWATPlus/Workflow/code_of_conduct.md"
-rm -r "$HOME/.SWAT/SWATPlus/Workflow/.github"
+rm "$SWAT_HOME/.SWAT/SWATPlus/Workflow/.gitattributes"
+rm "$SWAT_HOME/.SWAT/SWATPlus/Workflow/swatplus_aw.bat"
+rm "$SWAT_HOME/.SWAT/SWATPlus/Workflow/code_of_conduct.md"
+rm -r "$SWAT_HOME/.SWAT/SWATPlus/Workflow/.github"
 rm "./TauDEM5Bin_Linux.zip"
 rm "./v$version.zip"
 
 # set environmental variables
 echo "setting up environmental variables"
-echo 'export PATH=$PATH:$HOME/.SWAT/SWATPlus/Workflow' >>~/.bashrc
-echo 'export swatplus_wf_dir=$HOME/.SWAT/SWATPlus/Workflow' >>~/.bashrc
+echo 'export PATH=$PATH:$SWAT_HOME/.SWAT/SWATPlus/Workflow' >>~/.bashrc
+echo 'export swatplus_wf_dir=$SWAT_HOME/.SWAT/SWATPlus/Workflow' >>~/.bashrc
--- swatplus_aw.sh
+++ swatplus_aw.sh
@@ -2,18 +2,20 @@
 # set the directory of QGIS
 # export QGIS_Dir=C:\Program Files\QGIS 3.10
 # set ERRORLEVEL=0
+# set swat home
+export SWAT_HOME="/home/jph/dev/conda/pesteaux_conda/swat_tools"
 
 # set variables for project initialisation
-export python_script_prepare_qswat=$HOME/.SWAT/SWATPlus/Workflow/main_stages/prepare_project.py
-export python_script_make_config=$HOME/.SWAT/SWATPlus/Workflow/main_stages/generate_config.py
-export python_script_run_qswat=$HOME/.SWAT/SWATPlus/Workflow/main_stages/run_qswat.py
-export python_script_run_editor=$HOME/.SWAT/SWATPlus/Workflow/main_stages/run_editor.py
-export python_script_run_calibration=$HOME/.SWAT/SWATPlus/Workflow/main_stages/run_calibration.py
-export python_script_make_figures=$HOME/.SWAT/SWATPlus/Workflow/main_stages/run_make_figures.py
-export python_script_clean_up=$HOME/.SWAT/SWATPlus/Workflow/main_stages/run_clean_up.py
+export python_script_prepare_qswat=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/prepare_project.py
+export python_script_make_config=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/generate_config.py
+export python_script_run_qswat=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/run_qswat.py
+export python_script_run_editor=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/run_editor.py
+export python_script_run_calibration=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/run_calibration.py
+export python_script_make_figures=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/run_make_figures.py
+export python_script_clean_up=$SWAT_HOME/.SWAT/SWATPlus/Workflow/main_stages/run_clean_up.py
 
 export BASE_DIR=$PWD
-export swatplus_wf_dir=$HOME/.SWAT/SWATPlus/Workflow/
+export swatplus_wf_dir=$SWAT_HOME/.SWAT/SWATPlus/Workflow/
 # add qgis plugins folder to pythonpath
 export WF_QGIS=/usr/share/qgis/python/plugins
 
